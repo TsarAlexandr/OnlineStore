9c836327225b1b5fbe31eac3fce5e27aa2716728
Tsar Alexandr <tantal73180@gmail.com>
2018-10-23 12:01:00 +0400

    Change OrdersController & Orders/Create

--- WebDev_MainLab/Models/Order.cs
+++ WebDev_MainLab/Models/Order.cs
@@ -6,8 +6,8 @@ namespace WebDev_MainLab.Models
     public class Order
     {
         public int ID { get; set; }
-        public Country Country { get; set; }
-        public State State{ get; set; }
+        public int CountryID { get; set; }
+        public int StateID{ get; set; }
         [Required]
         [MaxLength(20)]
         public string Adress { get; set; }
