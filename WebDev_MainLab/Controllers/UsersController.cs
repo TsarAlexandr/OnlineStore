545d783f85bc2a122e902a904791d03c80f251a5
Tsar Alexandr <tantal73180@gmail.com>
2018-11-23 11:43:11 +0300

    Add UsersController and UserViews

---
+++ WebDev_MainLab/Controllers/UsersController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebDev_MainLab.Data;
+using WebDev_MainLab.Models;
+
+namespace WebDev_MainLab.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public IActionResult Index()
+        {
+            return View(_context.Users.ToList());
+        }
+
+        
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Country
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var country = await _context.Country.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Country.Remove(country);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CountryExists(int id)
+        {
+            return _context.Country.Any(e => e.ID == id);
+        }
+    }
+}
